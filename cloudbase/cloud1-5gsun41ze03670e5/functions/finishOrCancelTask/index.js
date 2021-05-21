131c0f189a380625702c55258685054f37f61054
honeysyt <honeysyt@foxmail.com>
2021-05-21 19:20:14 +0800

    update

--- cloudbase/cloud1-5gsun41ze03670e5/functions/finishOrCancelTask/index.js
+++ cloudbase/cloud1-5gsun41ze03670e5/functions/finishOrCancelTask/index.js
@@ -19,11 +19,13 @@ exports.main = async (event, context) => {
   type // 1 取消 2 完成
  } = event;
 
- const res = {
+ const resquest = {
   code: null,
   msg: ''
  };
 
+ // 1. 根据 date、id 更新对应 done 字段
+ // 2. 更新完成后再更新 allDone 字段
  await db.collection("task").where({
   _id: _id,
   'rules.dateList.date': date
@@ -31,20 +33,62 @@ exports.main = async (event, context) => {
   data: {
    'rules.dateList.$.done': type === 1 ? false : true
   }
- }).then(r => {
-  res.code = CODE_STATUS.SUCCESS;
-  res.msg = "更新成功";
- }).catch((err) => {
-  res.code = CODE_STATUS.ERROR;
-  res.msg = "更新失败：" + err;
- });
+ }).then(async () => {
 
+  const res = await getCollectionById(_id);
+  console.log(res)
+  // await db.collection("task").where({
+  //  _id: _id
+  // }).get().then(async res => {
+
+   if (res.data.length) {
+    let o = res.data[0];
+    const allDone = o.rules.dateList.every(i => i.done === true);
+    console.log(allDone, o.rules.allDone);
+
+    if (allDone !== o.rules.allDone) {
+     await updateAllDoneById(_id);
+     // await db.collection("task").where({
+     //  _id: _id
+     // }).update({
+     //  data: {
+     //   'rules.allDone': allDone
+     //  }
+     // }).then(() => {
+      resquest.code = CODE_STATUS.SUCCESS;
+      resquest.msg = "更新成功";
+     // });
+    }
+   }
+  // });
+ }).catch((err) => {
+  resquest.code = CODE_STATUS.ERROR;
+  resquest.msg = "更新失败：" + err;
+ });
 
  return {
   event,
-  res,
+  resquest,
   openid: wxContext.OPENID,
   appid: wxContext.APPID,
   unionid: wxContext.UNIONID,
  }
+}
+
+async function updateAllDoneById(id) {
+ console.log('update alldone')
+ return await db.collection("task").where({
+  _id: id
+ }).update({
+  data: {
+   'rules.allDone': allDone
+  }
+ });
+}
+
+async function getCollectionById(id) {
+ console.log('query by id')
+ return await db.collection("task").where({
+  _id: id
+ }).get();
 }
