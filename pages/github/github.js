8358b4cbef34c6e294afcc8f340a2b6d1174c1b7
honeysyt <honeysyt@foxmail.com>
2021-05-24 18:25:42 +0800

    fix: perfect layout

--- pages/github/github.js
+++ pages/github/github.js
@@ -1,23 +1,78 @@
 // pages/github/github.js
-Component({
- /**
-  * 组件的属性列表
-  */
- properties: {
-
- },
+Page({
 
  /**
-  * 组件的初始数据
+  * 页面的初始数据
   */
  data: {
+  url: "https://github.com/syt-honey/Reminder"
+ },
+
+ /**
+  * 生命周期函数--监听页面加载
+  */
+ onLoad: function (options) {
+
+ },
+
+ toCopyLink() {
+  const url = this.data.url;
+  wx.setClipboardData({
+   data: url,
+   success: function (res) {
+    wx.showToast({
+     title: '复制成功',
+    });
+   }
+  });
+ },
+
+ /**
+  * 生命周期函数--监听页面初次渲染完成
+  */
+ onReady: function () {
 
  },
 
  /**
-  * 组件的方法列表
+  * 生命周期函数--监听页面显示
   */
- methods: {
+ onShow: function () {
+
+ },
+
+ /**
+  * 生命周期函数--监听页面隐藏
+  */
+ onHide: function () {
+
+ },
+
+ /**
+  * 生命周期函数--监听页面卸载
+  */
+ onUnload: function () {
+
+ },
+
+ /**
+  * 页面相关事件处理函数--监听用户下拉动作
+  */
+ onPullDownRefresh: function () {
+
+ },
+
+ /**
+  * 页面上拉触底事件的处理函数
+  */
+ onReachBottom: function () {
+
+ },
+
+ /**
+  * 用户点击右上角分享
+  */
+ onShareAppMessage: function () {
 
  }
 })
